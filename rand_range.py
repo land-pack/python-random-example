439e4a00c6f7fdc9ea54e6f0d842391cbb297223
landpack <ada.frank.ak@gmail.com>
2017-07-04 12:15:08 +0800

    [add] sample & range

---
+++ rand_range.py
@@ -0,0 +1,7 @@
+import random
+
+
+random.seed(10)
+
+for i in range(100):
+    print(random.randrange(1, 11, 2))
---
+++ rand_sample.py
@@ -0,0 +1,9 @@
+import random
+
+random.seed(10)
+
+a= [1,2,3,4,5,6,7,8,9]
+
+
+
+print(random.sample(a, 3))
